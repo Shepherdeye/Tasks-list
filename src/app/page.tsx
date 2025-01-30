5b47df4b42382a1d640f2d271ee10fdd46cffedd
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-01-30 17:06:29 +0200

    add task table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import prisma from '@/utils/db'
 import Link from 'next/link'
 import React from 'react'
 
-const HomePage = () => {
-
+const HomePage = async () => {
+  const tasks = await prisma.task.findMany();
   return (
     <div className='w-full flex flex-col justify-center items-center gap-7 py-10'>
       <div className='w-8/12 flex justify-between items-center' >
@@ -15,8 +16,36 @@ const HomePage = () => {
           </Link>
         </div>
       </div>
-      <div>
-        this is for table
+      <div className='container'>
+        <table className="table w-full text-left mt-5">
+          <thead className="border-t-2 border-b-2 border-gray-300 text-xl">
+            <tr>
+              <th className="p-3">#</th>
+              <th>Task Title</th>
+              <th>Task Status</th>
+              <th>Task Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task, index) => (
+              <tr key={task.id} className="border-b border-gray-500">
+                <td className="p-3">{index + 1}</td>
+                <td>{task.title}</td>
+                <td>
+                  {task.status.toString()}
+                </td>
+                <td>
+                  <Link
+                    href={`/task/${task.id}`}
+                    className="bg-blue-600 hover:bg-blue-800 transition-colors text-white rounded-md p-2"
+                  >
+                    Details
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   )
