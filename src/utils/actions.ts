8edd943579b43c682eccd331985f3f980a4184d0
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2025-02-04 00:42:12 +0200

    update function

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -3,6 +3,8 @@ import { CreateTaskDto } from './dtos';
 import { revalidatePath } from "next/cache";
 import prisma from "./db";
 import { redirect } from "next/navigation";
+import { Status } from '@prisma/client';
+import { error } from 'console';
 
 export async function createTask({ title, description }: CreateTaskDto) {
     if (typeof title !== 'string' || title.length < 2) return;
@@ -33,4 +35,31 @@ export async function deleteTask(formData: FormData) {
     }
     revalidatePath("/");
     redirect("/");
+}
+
+export async function updateTask(formData: FormData) {
+
+    const id = formData.get("id")?.toString();
+    const title = formData.get("title")?.toString();
+    const description = formData.get("description")?.toString();
+    const status = formData.get("status") as Status;
+
+    if (!id) return;
+    if (!title) return;
+    if (!description) return;
+    if (!status) return;
+
+    try {
+        await prisma.task.update({
+            where: { id: parseInt(id) },
+            data: {
+                title,
+                description,
+                status
+            }
+        })
+    } catch {
+        throw new Error("failed to update task");
+    }
+
 }
